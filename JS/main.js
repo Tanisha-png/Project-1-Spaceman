0cddd5b4e8be761bb50534ddf494481f4ff65906
Tanisha-png <user@email.com>
2024-11-28 08:11:35 -0500

    Update Code

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,38 @@
 /*----- constants -----*/
 
-const characters = ['Mulan', 'Tiana', 'Genie', 'Simba']
+const winningCharacters = ['Mulan', 'Tiana', 'Genie', 'Simba']
 
 /*----- state variables -----*/
 
 let winner;
 let turn;
 let player;
-let letters;
+let square;
 
 /*----- cached elements  -----*/
+const messageEl = document.querySelector('#message');
+const playAgainEl = document.querySelector('#play-again');
+const buttonEls = document.querySelectorAll('.btn-letter > div')
 
 
 /*----- event listeners -----*/
+playAgainEl.addEventListener('click', init);
 
 
 /*----- functions -----*/
 init();
+
+function init() {
+    square = ['', '', '', ''];
+    turn = '';
+    winner = false;
+    tie = false;
+    render();
+}
+
+function render() {
+    renderSquare();
+    renderMessages();
+};
+
+
